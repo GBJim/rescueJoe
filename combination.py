98e09c64643563d2fdd5c50385edc2ffb7ff3597
GBJim <ccha97u@gmail.com>
2015-03-27 02:01:43 +0800

    2.0, Much more Faste, less memory

--- combination.py
+++ combination.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def getCombination(n):
 	c = (n*n -n)/2
-	A = np.zeros(c,dtype = np.uint16)
-	B = np.zeros(c,dtype = np.uint16)
+	A = np.zeros([c,2],dtype = np.uint16)
+	
 	addingArray = np.array(range(1,n), dtype=np.uint16)
 	start = 0
 	end = 0
@@ -12,7 +12,7 @@ def getCombination(n):
 	for element in range(n-1):
 		start = end
 		end = start + n-1 - element
-		A[start:end] = element
+		A[:,0][start:end] = element
 		
 	print("Array A finished")
 	
@@ -27,9 +27,9 @@ def getCombination(n):
 		
 		
 		
-		B[start:end] = addingArray[element:]
+		A[:,1][start:end] = addingArray[element:]
 		
 	print("Array B finished")
-	return np.dstack((A,B))
+	return A
 
 x = getCombination(50000)
